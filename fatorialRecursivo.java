ef70e2192391115aca9c433b87b1245d80bda112
Pedro <pedrodeffantijr@gmail.com>
2022-02-09 10:36:14 -0300

    Fatorial Recursivo implementado

---
+++ fatorialRecursivo.java
@@ -0,0 +1,10 @@
+public class FatorialRecursivo {
+          public static in calcularFatorialRecursivo( int n ) throws IlligalArgumentException {
+	if (n ==)
+	         return 1;
+	return n * calcularFatorialRecursivo(n-1)
+	}
+	public static void main(String[] args) {
+	        System.out.println(calcularFatorialRecursivo(5);
+	}
+}
